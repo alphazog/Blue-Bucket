107a4ce96c583ca9f1c14a416301a705d3de80d3
Alex <>
2018-02-16 22:39:08 +0200

    Implemented essential interface

---
+++ Blue Bucket/Modules/FeedItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedItemTableViewCell.swift
+//  Blue Bucket
+//
+//  Created by Alex on 2/13/18.
+//  Copyright © 2018 zogincorprorated. All rights reserved.
+//
+
+import UIKit
+
+class FeedItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
